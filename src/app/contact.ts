65541b38a122349e3257e60f098c6e4e84ffa54c
Yesenia <eliyessie@gmail.com>
2018-07-11 12:28:13 -0700

    class exercises

---
+++ src/app/contact.ts
@@ -0,0 +1,7 @@
+export class Contact {
+    id: number;
+    name: string;
+    phone: string;
+    email: string;
+}
+// this is a base schema, or template. (predefinning key values like a database)
