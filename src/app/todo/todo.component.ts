c2591b5b3b5329d85122bb1187042082e7bedcb8
Yesenia <eliyessie@gmail.com>
2018-07-16 12:43:26 -0700

    finished todo-list

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TODOS } from '../mock-todo';
+import { Todo } from '../todo';
 
 @Component({
   selector: 'app-todo',
@@ -8,8 +9,27 @@ import { TODOS } from '../mock-todo';
 })
 export class TodoComponent implements OnInit {
 
+  newTask: Todo = new Todo();
   todos = TODOS;
+
   constructor() { }
+
+  addTodo() {
+    const lastIndex = this.todos.length - 1;
+    const last = this.todos[lastIndex];
+    this.newTask.id = last.id + 1;
+    this.newTask.status = false;
+    this.newTask.trash = false;
+    this.todos.push(this.newTask);
+    this.newTask = new Todo();
+  }
+
+  toggleTodo(item) {
+    item.status = !item.status; // this function here is used to toggle the on/off, true/false
+  }
+  moveToTrash(item) {
+    item.trash = !item.trash; // this moves the items into the trash modal
+  }
 
   ngOnInit() {
   }
