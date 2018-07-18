1dafd101528220a3a2b92568672c5ec454586709
Yesenia <eliyessie@gmail.com>
2018-07-17 20:55:48 -0700

    added the list of heroes and villains.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { TodoComponent } from './todo/todo.component';
+import { HeroComponent } from './hero/hero.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent},
-  { path: 'contact', component: ContactComponent},
-  { path: 'todo', component: TodoComponent}  // the open path here will redirect the page to the home component view
+  { path: '', component: HomeComponent },
+  // the open path here will redirect the page to the home component view
+  { path: 'contact', component: ContactComponent },
+  { path: 'todo', component: TodoComponent },
+  { path: 'hero', component: HeroComponent },
+  { path: 'hero/:id', component: HeroDetailComponent }
 ];
 
 @NgModule({
---
+++ src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HEROES } from '../mock-heroes';
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css']
+})
+export class HeroDetailComponent implements OnInit {
+
+  hero: object;
+  private temp: any; // this is so we can store the end part of url into a variable temporarily
+  constructor ( private route: ActivatedRoute ) { }
+
+  ngOnInit ( ) {
+    this.temp = this.route.params.subscribe( params => {
+      HEROES.forEach( (el) => {
+        if ( el.id === + params ['id'] ) {
+          console.log( el );
+          this.hero = el;
+        }
+      });
+    });
+  }
+
+}
